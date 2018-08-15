1d36325288f2a0d44df7dcabf9364661814d62cd
PrestonWinstead <prestonwinstead@me.com>
2018-08-15 14:48:16 -0500

    bug(marker): prevent crash on remove

--- index.js
+++ index.js
@@ -322,12 +322,14 @@ module.exports = function(gMapsApi) {
    */
   MarkerLabel_.prototype.onRemove = function () {
     var i;
-    this.labelDiv_.parentNode.removeChild(this.labelDiv_);
-    this.eventDiv_.parentNode.removeChild(this.eventDiv_);
+    if (this.labelDiv_ && this.labelDiv_.parentNode) {
+      this.labelDiv_.parentNode.removeChild(this.labelDiv_);
+      this.eventDiv_.parentNode.removeChild(this.eventDiv_);
 
-    // Remove event listeners:
-    for (i = 0; i < this.listeners_.length; i++) {
-      gMapsApi.event.removeListener(this.listeners_[i]);
+      // Remove event listeners:
+      for (i = 0; i < this.listeners_.length; i++) {
+        google.maps.event.removeListener(this.listeners_[i]);
+      }
     }
   };
 
